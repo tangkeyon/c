a061dd1b6a36f7a7aa1823f337d2e652bbca9bd9
tangkeyon <tangkeyon@foxmail.com>
2021-01-20 22:35:27 +0800

    初始

---
+++ day01/从窗口获取输入.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+//使用变量之前先声明
+  int num;
+
+  printf("please enter your inf0:\n");
+//要求以十进制整数的方式进行输入 将输入的数据放到num的地址   
+  scanf("%d",&num);
+  printf("your info: %d\n",num);
+
+
+
+
+
+
+    return 0;
+}
